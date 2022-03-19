6d2ab41c3e54757361b8f72ccbab2d336c40c453
9etrichor <dick2452683247@gmail.com>
2022-03-19 11:37:51 +0800

    complete a round competition, ourput string of different combination, change of score

--- rps.js
+++ rps.js
@@ -1,7 +1,8 @@
 /* computer random choices */
 
 const choices = ["Rock", "Paper", "Scissors"]
-
+let playerScore = 0;
+let computerScore = 0;
 
 function computer(){
     return choices[Math.floor(Math.random() * 3)];
@@ -12,13 +13,81 @@ function computer(){
 function player(){
     return String(prompt("Rock/Paper/Scissors"));
 }
-console.log(player());
+
 
 
 /* decide one round who wim who lose, ourput string like"Rock beat scissors" */
 function round(){
+    let computerChoices = computer();
+    let playerChoices = player();
+    let roundInfo = "";
+    switch(playerChoices.toLowerCase())
+    {
+        /*when you choices rock start */
+        case "rock":
+            switch(computerChoices.toLowerCase())
+            {
+                case "rock":
+                    roundInfo = `${playerChoices} to Rock, keep going on`;
+                    break;
+                case "paper":
+                    computerScore = computerScore + 1;
+                    roundInfo = `${playerChoices} to Paper, you lose`;
+                    break;
+                case "scissors":
+                    playerScore = playerScore + 1;
+                    roundInfo = `${playerChoices} to scissors, you win`;
+                    break;
+            }
+            break;
+        /*when you choices rock end */
 
+        /*when you choices paper start */
+        case "paper":
+            switch(computerChoices.toLowerCase())
+            {
+                case "rock":
+                    playerScore = playerScore + 1;
+                    roundInfo = `${playerChoices} to Rock, you win`;
+                    break;
+                case "paper":
+                    roundInfo = `${playerChoices} to Paper, keep going on`;
+                    break;
+                case "scissors":
+                    computerScore = computerScore + 1;
+                    roundInfo = `${playerChoices} to scissors, you lose`;
+                    break;
+            }
+            break;
+        /*when you choices paper end */
+        
+        /*when you choices scissors start*/
+        case "scissors":
+            switch(computerChoices.toLowerCase())
+            {
+                case "rock":
+                    computerScore = computerScore + 1;
+                    roundInfo = `${playerChoices} to Rock, you lose`;
+                    break;
+                case "paper":
+                    playerScore = playerScore + 1;
+                    roundInfo = `${playerChoices} to Paper, you win`;
+                    break;
+                case "scissors":
+                    roundInfo = `${playerChoices} to scissors, keep going on`;
+                    break;
+            }
+            break;
+        /*when you choices scissors end*/
+        
+    }
+    return roundInfo;
+    
+    
 }
+console.log(round());
+console.log(playerScore);
+console.log(computerScore);
 
 /* decide how many rounds, final winner, count number of win */
 function manyRounds(){
