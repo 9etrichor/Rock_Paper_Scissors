f40c1f6bf9c7340716cdc925184ed971ea923e07
9etrichor <dick2452683247@gmail.com>
2022-03-26 11:57:58 +0800

    add function show correct image on player and computer side, when player click and start a round

--- rps_test.js
+++ rps_test.js
@@ -1,11 +1,17 @@
-let playerChoice = "paper";
+let playerChoice = "";
 let computerChoice = "";
 let playerScore = 0;
 let computerScore = 0;
 let outputStr = "";
+/*get three button element */
 const btnRock = document.getElementById('btnRock');
 const btnPaper = document.getElementById('btnPaper');
 const btnSci = document.getElementById('btnSci');
+/*get three button element */
+
+const playerImg = document.getElementById("playerimg");
+const computerImg = document.getElementById("computerimg");
+
 /*computer make choice start */
 const choices = ["rock", "paper", "scissors"]
 function computer(){
@@ -75,24 +81,60 @@ function singleRound()
 };
 /*play a round end */
 
+/* output correct image when computer make choice start */
+function computerImage()
+{
+    switch(computerChoice)
+    {
+        case "rock":
+            return "img_pack/rock.png";
+            break;
+        case "paper":
+            return "img_pack/Paper.png";
+            break;
+        case "scissors":
+            return "img_pack/scissors_new.png";
+            break;
+    }
+}
+/* output correct image when computer make choice end */
+
+
+/*player click button to make choice start*/
+/* player click and choice rock start */
 btnRock.addEventListener('click', function(){
+    playerImg.src = "img_pack/rock.png";
     playerChoice = "rock";
     computerChoice = computer();
+    computerImg.src = computerImage();
     outputStr = singleRound();
     console.log(outputStr + playerScore);
 });
+/* player click and choice rock end */
+
+
+/* player click and choice paper start */
 
 btnPaper.addEventListener('click', function(){
+    playerImg.src = "img_pack/Paper.png";
     playerChoice = "paper";
     computerChoice = computer();
+    computerImg.src = computerImage();
     outputStr = singleRound();
     console.log(outputStr + playerScore);
 });
+/* player click and choice paper end */
 
+
+/* player click and choice scissors start */
 btnSci.addEventListener('click', function(){
+    playerImg.src = "img_pack/scissors_new.png";
     playerChoice = "scissors";
     computerChoice = computer();
+    computerImg.src = computerImage();
     outputStr = singleRound();
     console.log(outputStr + playerScore);
 });
-console.log(playerScore);
+/* player click and choice scissors end */
+/*player click button to make choice end*/
+
